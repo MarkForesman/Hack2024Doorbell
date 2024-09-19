02d1ad0aa594f40385f517b1c4cbb48b5281a1a2
connorwehrum_microsoft <connorwehrum@microsoft.com>
2024-09-19 09:10:30 -0700

    add stuff to button 2

--- TestApp/label_scanner.py
+++ TestApp/label_scanner.py
@@ -41,9 +41,27 @@ class Scanner:
         self.update_color(1, 0, 0, 0)
 
     def button_2_press(self):
-        self.update_color(1, 1, 0, 0) 
-        print("Button 2 pressed")
-        # camera.capture_file('image2.jpg')
+        guid_filename = f"{uuid.uuid4()}.jpg"
+        print("Button 1 pressed")
+        camera.capture_file(guid_filename)
+        self.update_color(1, 255, 255, 0) 
+        result: dict = self.iothub.upload_blob_file(guid_filename)
+        print("Output:", result)
+        if result.get("status_code") is not 200:
+            self.update_color(1, 1, 0, 0)
+            self.update_color(2, 1, 0, 0)
+            raise Exception(f"Process failed! {result}")
+        event=self.generate_button_event(1, guid_filename)
+        event_json=event.model_dump_json()
+        self.iothub.send_message(event_json)
+        self.update_color(1, 0, 1, 0)
+        if os.path.exists(guid_filename):
+            os.remove(guid_filename)
+            print(f"{guid_filename} has been deleted.")
+        else:
+            print(f"{guid_filename} does not exist.")
+        time.sleep(3)
+        self.update_color(1, 0, 0, 0)
 
     def generate_button_event(self, button, file_name):
         return ButtonPressEvent(Button=button, DeviceType="LabelScanner", DeviceId=self.iothub.device_id, Metadata=file_name)     
